94ca9eadcb50ef1bb26a2f21e30b7e201d7b2456
Dzikry123 <96764302+Dzikry123@users.noreply.github.com>
2023-08-11 19:55:03 +0700

    Update DetailNotification.js

--- src/components/Users/DetailNotification.js
+++ src/components/Users/DetailNotification.js
@@ -17,7 +17,7 @@ const DetailNotification = () => {
 
   const getNotifications = async () => {
     try {
-      const response = await axios.get(`http://localhost:5000/messages/${id}`)
+      const response = await axios.get(`https://hotel-backend-nine.vercel.app/messages/${id}`)
       setMessages(response.data)
       console.log(response.data);
     } catch (error) {
